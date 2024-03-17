3dd43c24b5869254704a9d896522b2b8018c5512
Titas Tupinis <t.tupinis@gmail.com>
2024-03-17 10:39:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-#main file
+computer_cards_in_play = []
+user_cards_in_play = []
+
+def computer_move():
+    computer_cards_in_play += computer_deck[0].face_up(True)
+    computer_deck.pop(0) 
+
+def user_move():
+    if input("Press 'Enter' to make a move"):
+        computer_cards_in_play += computer_deck[0].face_up(True)
+        computer_deck.pop(0)
+
 def compare_last_cards():
     if computer_cards_in_play[-1:].rank > user_cards_in_play[-1:].rank:
         return_cards("computer") # merge both cards in play, shuffle and append to computer's deck
@@ -6,8 +17,6 @@ def compare_last_cards():
         return_cards("user") # merge both cards in play, shuffle and append to user's deck
     elif computer_cards_in_play[-1:].rank == user_cards_in_play[-1:].rank:
         war() # both place a face down card and face up card, check again
-
- 
 
 def war():
     computer_cards_in_play += computer_deck[0]
@@ -17,8 +26,6 @@ def war():
     user_deck.pop(0)
     user_move()
     compare_last_cards()
-
- 
 
 def return_cards(winner):
     return_pile += computer_cards_in_play + user_cards_in_play
@@ -30,8 +37,6 @@ def return_cards(winner):
     if winner == "user":
         user_deck += return_pile
 
- 
-
 def print_cards_in_play():
     print("Computer: ", end="")
     for card in computer_cards_in_play:
@@ -41,8 +46,6 @@ def print_cards_in_play():
     for card in user_cards_in_play:
         print(card, end="")
     print()
-
- 
 
 def main():
     computer_move()
